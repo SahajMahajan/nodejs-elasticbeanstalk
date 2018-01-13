64564bff606dddea67dac10054020d68436ec414
EC2 Default User <ec2-user@ip-172-31-35-134.ec2.internal>
2018-01-13 05:44:28 +0000

    Server.Js modified again

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express'),
     port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-  res.send('Hello world This is my First  Node JS App')
+  res.send('Hello world This is my New  Node JS App')
 })
 
 app.listen(port, function(){
